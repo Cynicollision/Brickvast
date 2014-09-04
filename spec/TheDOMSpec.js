9b4f4e7f8016ce1761bd6b1d2f08d2adfc2087af
Sean Normoyle <stn825@gmail.com>
2014-09-04 18:22:17 -0400

    organization

--- spec/TheDOMSpec.js
+++ spec/TheDOMSpec.js
@@ -1,12 +1,1 @@
-﻿//************************************************************************************//
-// Test suite for TheDOM class
-//************************************************************************************//
-
-describe('TheDOM: can find the main canvas', function () {
-    it('uses jQuery to retrieve a valid Canvas element and calling getContext(\'2d\')', function () {
-        var theCanvas = $("#theCanvas");
-
-        // assert
-        expect(theCanvas[0].getContext('2d')).toBeDefined();
-    })
-});
+﻿
