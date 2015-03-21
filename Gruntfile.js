54e0affe27850555ff8fe892ddc147fb35c62854
Sean Normoyle <stn825@gmail.com>
2015-03-21 17:22:28 -0400

    switched to uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,18 +17,6 @@
             }
         },
 
-        'closure-compiler': {
-            engine: {
-                closurePath: 'C:\\Users\\Sean\\workspace\\vastengine\\engine\\',
-                js: 'engine/src/*',
-                jsOutputFile: 'engine/build/vastengine.js',
-                maxBuffer: 500,
-                options: {
-                    language_in: 'ECMASCRIPT5_STRICT'
-                }
-            }
-        },
-
         clean: {
             'engine/build': ['engine/build/vastengine.js']
         },
@@ -57,25 +45,28 @@
                 }
             }
         },
-		
-		exec: {
-		  echo_something: 'echo "This could be fun"'
-		}
+
+        uglify: {
+            build: {
+                files: {
+                    'engine/build/vastengine.js': ['engine/src/*.js']
+                }
+            }
+        }
 		
     });
 
-    grunt.loadNpmTasks('grunt-closure-compiler');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-jsdoc');
-	grunt.loadNpmTasks('grunt-exec');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     
 
     // register the tasks
     grunt.registerTask('test', ['jshint', 'jasmine']);
-	grunt.registerTask('build', ['clean', 'closure-compiler', 'copy']);
+	grunt.registerTask('build', ['clean', 'uglify', 'copy']);
     grunt.registerTask('doc', 'jsdoc');
 	
 	// might be handy someday
